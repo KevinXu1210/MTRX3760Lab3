8c6fda115e8ab752fc546e2d6018a86fa6415eb7
KevinXu1210 <yike1210@gmail.com>
2023-09-07 17:22:45 +1000

    rest function

--- Set/OccupancyMapHash.cpp
+++ Set/OccupancyMapHash.cpp
@@ -5,3 +5,21 @@
 COccupancyMapHash::COccupancyMapHash()
 {
 }
+
+// Return the name of the approach as a string, for display purposes
+std::string COccupancyMapHash::GetNameOfApproach()
+{
+    return "Set-Base";
+}
+
+// Add a location observed to be occupied to the map
+void COccupancyMapHash::AddOccupiedLocation(std::pair<int,int> Location)
+{
+    mOccupancyGrid.insert(Location);
+}
+
+// Check if a location is occupied
+bool COccupancyMapHash::CheckIsOccupied(std::pair<int,int> Location)
+{
+    return mOccupancyGrid.find(Location) != mOccupancyGrid.end();
+}
