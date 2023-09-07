2a7a8aa6b468b2e16fcc53c95a4677b13f53d33b
KevinXu1210 <yike1210@gmail.com>
2023-09-07 17:26:58 +1000

    class

--- 2D_Vector/OccupancyMapHash.h
+++ 2D_Vector/OccupancyMapHash.h
@@ -4,3 +4,11 @@
 #include <string>
 #include <vector>
 #include "OccupancyMapBase.h"
+
+// Class to implement a occupancy map using a vector
+class COccupancyMapHash : public COccupancyMapBase
+{
+
+};
+
+#endif
