03f44b53a7fe390dd595ae0ef4334a17fe0722b6
KevinXu1210 <yike1210@gmail.com>
2023-09-07 17:20:27 +1000

    class

--- Set/OccupancyMapHash.h
+++ Set/OccupancyMapHash.h
@@ -4,3 +4,26 @@
 #include <string>
 #include <set>
 #include "OccupancyMapBase.h"
+
+// Class to implement a occupancy map using set
+class COccupancyMapHash : public COccupancyMapBase
+{
+
+  public:
+    COccupancyMapHash();
+    //virtual ~COccupancyMapHash(){};
+    
+    // Return the name of the approach as a string, for display purposes
+    std::string GetNameOfApproach() override; 
+    
+    // Add a location observed to be occupied to the map
+    void AddOccupiedLocation(std::pair<int,int> Location) override;
+    
+    // Check if a location is occupied
+    bool CheckIsOccupied(std::pair<int,int> Location) override;
+
+
+
+};
+
+#endif
