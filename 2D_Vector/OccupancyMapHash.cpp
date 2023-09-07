da13ed9f740553d9fc5c6003317434174c62d009
KevinXu1210 <yike1210@gmail.com>
2023-09-07 17:26:20 +1000

    header

---
+++ 2D_Vector/OccupancyMapHash.cpp
@@ -0,0 +1,3 @@
+#include "OccupancyMapHash.h"
+
+const int MAX_SIZE = 2048; // MAX_SIZE is the maximum size of the map
---
+++ 2D_Vector/OccupancyMapHash.h
@@ -0,0 +1,6 @@
+#ifndef _OCCUPANCYMAPVECTOR_H
+#define _OCCUPANCYMAPVECTOR_H
+
+#include <string>
+#include <vector>
+#include "OccupancyMapBase.h"
